56a6cd3dce6da9ae1a700f4c7712b10ab27d8921
Olga121318 <olga121318@gmail.com>
2020-12-17 19:18:57 +0300

    Блог о Кафе

--- js/script.js
+++ js/script.js
@@ -61,6 +61,7 @@ let cardsData = [
     }
 ]; 
 
+
 let makeElement = function (tagName, className, text) {
   let element = document.createElement(tagName);
   element.classList.add(className);
@@ -78,7 +79,7 @@ let createCard = function (service) {
     listItem.appendChild(picture);
 
 
-    let title = makeElement('h2', 'service__title', service.title);
+    let title = makeElement('h3', 'service__title', service.title);
     listItem.appendChild(title);
 
     let text = makeElement('p', 'service__text', service.text);
@@ -93,3 +94,55 @@ for (let i = 0; i < cardsData.length; i++) {
     let cardItem = createCard(cardsData[i]);
     cardList.appendChild(cardItem);
 };
+
+let newCardsData = [
+    {
+        imgUrl: 'img/blog-1-img-2.jpg',
+        title: 'ПЕРЕГОВОРЫ',
+        text: 'Теория эманации диссонирует незначительный эйдос. Миракль образует конструктивный импрессионизм. Пушкин подарил Гоголю фабулу "Мертвых душ" не потому, что художественное переживание изящно продолжает самодостаточный миракль. Богатство мировой литературы от Платона до Ортеги-и-Гассета свидетельствует о том, что канон аккумулирует невротический "кодекс деяний".',
+        metaLink: 'ЧИТАТЬ ДАЛЕЕ...'
+    },
+    {
+        imgUrl: 'img/blog-1-img-1.jpg',
+        title: 'МАСТЕР-КЛАСС',
+        text: 'Бизнес-стратегия отражает целевой сегмент рынка. Производство вполне выполнимо. Медиабизнес индуцирует типичный метод изучения рынка. Баланс спроса и предложения вырождена. Стимулирование сбыта продуцирует рекламный бриф. К тому же стратегическое планирование переворачивает нишевый проект.',
+        metaLink: 'ЧИТАТЬ ДАЛЕЕ...'
+    },
+    {
+        imgUrl: 'img/blog-1-img-3.jpg',
+        title: 'ДЕГУСТАЦИЯ',
+        text: 'Проникновение глубинных магм смещает меандр. Согласно Венинг-Мейенсу, элювиальное образование существенно прекращает грязевой вулкан, что позволяет проследить соответствующий денудационный уровень. Фирн извержен. Исследование указанной связи должно опираться на тот факт, что эоловое засоление разогревает бараний лоб.',
+        metaLink: 'ЧИТАТЬ ДАЛЕЕ...'
+    }
+];
+let newCreateCard = function (description) {
+    let newListItem = makeElement('li', 'description');
+
+    let picture = makeElement('img', 'description__image');
+    picture.src = description.imgUrl;
+    picture.alt = description.title;
+    newListItem.appendChild(picture);
+
+
+    let title = makeElement('a', 'description__title', description.title);
+    title.href = '#';
+    newListItem.appendChild(title);
+
+    let text = makeElement('p', 'description__text', description.text);
+    newListItem.appendChild(text);
+
+    let metaLink = makeElement('a', 'description__link', description.metaLink);
+    metaLink.href = '#';
+    newListItem.appendChild(metaLink);
+
+    return newListItem;
+};
+
+let newCardList = document.querySelector('.descriptions');
+
+for (let i = 0; i < newCardsData.length; i++) {
+    let newCardItem = newCreateCard(newCardsData[i]);
+    newCardList.appendChild(newCardItem);
+};
+
+
